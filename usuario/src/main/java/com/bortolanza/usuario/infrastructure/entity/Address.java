3e7e131379a06c1337b30b14af3c51181eb42b0c
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-04-27 16:36:22 -0300

    adding security classes

---
+++ usuario/src/main/java/com/bortolanza/usuario/infrastructure/entity/Address.java
@@ -0,0 +1,34 @@
+package com.bortolanza.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "number")
+    private Long number;
+    @Column(name = "complement", length = 100)
+    private String complement;
+    @Column(name = "city", length = 150)
+    private String city;
+    @Column(name = "state", length = 2)
+    private String state;
+    @Column(name = "zip_code", length = 9)
+    private String zipCode;
+
+
+    }
