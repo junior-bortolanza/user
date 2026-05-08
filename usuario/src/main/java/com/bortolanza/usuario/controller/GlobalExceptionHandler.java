299bac2d63c1202411f7e5fb9fe74f172c7910cd
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-05-08 14:12:34 -0300

    "Refactored login method and user authentication"

--- usuario/src/main/java/com/bortolanza/usuario/controller/GlobalExceptionHandler.java
+++ usuario/src/main/java/com/bortolanza/usuario/controller/GlobalExceptionHandler.java
@@ -5,31 +5,62 @@ import com.bortolanza.usuario.infrastructure.exceptions.ConflictException;
 import com.bortolanza.usuario.infrastructure.exceptions.IllegalArgumentException;
 import com.bortolanza.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.bortolanza.usuario.infrastructure.exceptions.UnauthorizedException;
+import com.bortolanza.usuario.infrastructure.exceptions.dto.ErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex,
+                                                                            HttpServletRequest request){
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(buildErrorMessage(HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                "Not Found"
+                ));
     }
 
     @ExceptionHandler(ConflictException.class)
-    public ResponseEntity<String> handleConflictException(ConflictException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    public ResponseEntity<ErrorResponseDTO> handleConflictException(ConflictException ex, HttpServletRequest request){
+        return  ResponseEntity.status(HttpStatus.CONFLICT).body(buildErrorMessage(HttpStatus.CONFLICT.value(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                "Not Found"
+        ));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ErrorResponseDTO> handleUnauthorizedException(UnauthorizedException ex, HttpServletRequest request){
+        return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(buildErrorMessage(HttpStatus.UNAUTHORIZED.value(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                "User is not authorized"
+        ));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponseDTO> handleIllegalArgumentException(IllegalArgumentException ex, HttpServletRequest request){
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(buildErrorMessage(HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                "Bad Request"
+        ));
+    }
+
+    private ErrorResponseDTO buildErrorMessage(int status, String message, String path, String error) {
+        return ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .message(message)
+                .error(error)
+                .status(status)
+                .path(path)
+                .build();
     }
 }
